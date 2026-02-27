64b6e6125fc5c4f06ac86ce6ca8250716a5afe3b
Anikey Andrea <anikeyandrea20@gmail.com>
2026-02-26 23:58:36 -0600

    Avance Practica 3
    Faltan detalles

--- ConfInicial/Main_P3.cpp
+++ ConfInicial/Main_P3.cpp
@@ -1,9 +1,9 @@
 //
-// Previo 3
+// Práctica 3
 // 
-//Anikey Andrea Gomez Guzman
+// Anikey Andrea Gomez Guzman
 // 319323290
-//Fecha de entrega: 22 de Feb, 2026
+//Fecha de entrega: 27 de Feb, 2026
 
 #include<iostream>
 
@@ -78,100 +78,57 @@ int main() {
 	Shader ourShader("Shader/core.vs", "Shader/core.frag");
 
 
-	// Set up vertex data (and buffer(s)) and attribute pointers
-	// use with Orthographic Projection
+    // use with Perspective Projection
+    // El formato es: PosX, PosY, PosZ, BRILLO, Ignorado, Ignorado
+    float vertices[] = {
+        // Front face (Laterales - Brillo 0.85)
+        -0.5f, -0.5f, 0.5f,  0.85f, 0.0f, 0.0f,
+         0.5f, -0.5f, 0.5f,  0.85f, 0.0f, 0.0f,
+         0.5f,  0.5f, 0.5f,  0.85f, 0.0f, 0.0f,
+         0.5f,  0.5f, 0.5f,  0.85f, 0.0f, 0.0f,
+        -0.5f,  0.5f, 0.5f,  0.85f, 0.0f, 0.0f,
+        -0.5f, -0.5f, 0.5f,  0.85f, 0.0f, 0.0f,
 
-	//GLfloat vertices[] = {
- //      -0.5f*500, -0.5f, 0.5f, 1.0f, 0.0f,0.0f,//Front
-	//	0.5f * 500, -0.5f * 500, 0.5f * 500,  1.0f, 0.0f,0.0f,
-	//	0.5f * 500,  0.5f * 500, 0.5f * 500,  1.0f, 0.0f,0.0f,
-	//	0.5f * 500,  0.5f * 500, 0.5f * 500,  1.0f, 0.0f,0.0f,
-	//	-0.5f * 500,  0.5f * 500, 0.5f * 500, 1.0f, 0.0f,0.0f,
-	//	-0.5f * 500, -0.5f * 500, 0.5f * 500, 1.0f, 0.0f,0.0f,
-	//	
-	//    -0.5f * 500, -0.5f * 500,-0.5f * 500, 0.0f, 1.0f,0.0f,//Back
-	//	 0.5f * 500, -0.5f * 500,-0.5f * 500, 0.0f, 1.0f,0.0f,
-	//	 0.5f * 500,  0.5f * 500,-0.5f * 500, 0.0f, 1.0f,0.0f,
-	//	 0.5f * 500,  0.5f * 500,-0.5f * 500, 0.0f, 1.0f,0.0f,
-	//    -0.5f * 500,  0.5f * 500,-0.5f * 500, 0.0f, 1.0f,0.0f,
-	//    -0.5f * 500, -0.5f * 500,-0.5f * 500, 0.0f, 1.0f,0.0f,
-	//	
-	//	 0.5f * 500, -0.5f * 500,  0.5f * 500,  0.0f, 0.0f,1.0f,
-	//	 0.5f * 500, -0.5f * 500, -0.5f * 500,  0.0f, 0.0f,1.0f,
-	//	 0.5f * 500,  0.5f * 500, -0.5f * 500,  0.0f, 0.0f,1.0f,
-	//	 0.5f * 500,  0.5f * 500, -0.5f * 500,  0.0f, 0.0f,1.0f,
-	//	 0.5f * 500,  0.5f * 500,  0.5f * 500,  0.0f, 0.0f,1.0f,
-	//	 0.5f * 500,  -0.5f * 500, 0.5f * 500, 0.0f, 0.0f,1.0f,
- //     
-	//	-0.5f * 500,  0.5f * 500,  0.5f * 500,  1.0f, 1.0f,0.0f,
-	//	-0.5f * 500,  0.5f * 500, -0.5f * 500,  1.0f, 1.0f,0.0f,
-	//	-0.5f * 500, -0.5f * 500, -0.5f * 500,  1.0f, 1.0f,0.0f,
-	//	-0.5f * 500, -0.5f * 500, -0.5f * 500,  1.0f, 1.0f,0.0f,
-	//	-0.5f * 500, -0.5f * 500,  0.5f * 500,  1.0f, 1.0f,0.0f,
-	//	-0.5f * 500,  0.5f * 500,  0.5f * 500,  1.0f, 1.0f,0.0f,
-	//	
-	//	-0.5f * 500, -0.5f * 500, -0.5f * 500, 0.0f, 1.0f,1.0f,
-	//	0.5f * 500, -0.5f * 500, -0.5f * 500,  0.0f, 1.0f,1.0f,
-	//	0.5f * 500, -0.5f * 500,  0.5f * 500,  0.0f, 1.0f,1.0f,
-	//	0.5f * 500, -0.5f * 500,  0.5f * 500,  0.0f, 1.0f,1.0f,
-	//	-0.5f * 500, -0.5f * 500,  0.5f * 500, 0.0f, 1.0f,1.0f,
-	//	-0.5f * 500, -0.5f * 500, -0.5f * 500, 0.0f, 1.0f,1.0f,
-	//	
-	//	-0.5f * 500,  0.5f * 500, -0.5f * 500, 1.0f, 0.2f,0.5f,
-	//	0.5f * 500,  0.5f * 500, -0.5f * 500,  1.0f, 0.2f,0.5f,
-	//	0.5f * 500,  0.5f * 500,  0.5f * 500,  1.0f, 0.2f,0.5f,
-	//	0.5f * 500,  0.5f * 500,  0.5f * 500,  1.0f, 0.2f,0.5f,
-	//	-0.5f * 500,  0.5f * 500,  0.5f * 500, 1.0f, 0.2f,0.5f,
-	//	-0.5f * 500,  0.5f * 500, -0.5f * 500, 1.0f, 0.2f,0.5f,
-	//};
-	
+        // Back face (Laterales - Brillo 0.85)
+        -0.5f, -0.5f,-0.5f,  0.85f, 1.0f, 0.0f,
+         0.5f, -0.5f,-0.5f,  0.85f, 1.0f, 0.0f,
+         0.5f,  0.5f,-0.5f,  0.85f, 1.0f, 0.0f,
+         0.5f,  0.5f,-0.5f,  0.85f, 1.0f, 0.0f,
+        -0.5f,  0.5f,-0.5f,  0.85f, 1.0f, 0.0f,
+        -0.5f, -0.5f,-0.5f,  0.85f, 1.0f, 0.0f,
 
-	 //use with Perspective Projection
-	float vertices[] = {
-		-0.5f, -0.5f, 0.5f, 1.0f, 0.0f,0.0f,//Front
-		0.5f, -0.5f, 0.5f,  1.0f, 0.0f,0.0f,
-		0.5f,  0.5f, 0.5f,  1.0f, 0.0f,0.0f,
-		0.5f,  0.5f, 0.5f,  1.0f, 0.0f,0.0f,
-		-0.5f,  0.5f, 0.5f, 1.0f, 0.0f,0.0f,
-		-0.5f, -0.5f, 0.5f, 1.0f, 0.0f,0.0f,
-		
-	    -0.5f, -0.5f,-0.5f, 0.0f, 1.0f,0.0f,//Back
-		 0.5f, -0.5f,-0.5f, 0.0f, 1.0f,0.0f,
-		 0.5f,  0.5f,-0.5f, 0.0f, 1.0f,0.0f,
-		 0.5f,  0.5f,-0.5f, 0.0f, 1.0f,0.0f,
-	    -0.5f,  0.5f,-0.5f, 0.0f, 1.0f,0.0f,
-	    -0.5f, -0.5f,-0.5f, 0.0f, 1.0f,0.0f,
-		
-		 0.5f, -0.5f,  0.5f,  0.0f, 0.0f,1.0f,
-		 0.5f, -0.5f, -0.5f,  0.0f, 0.0f,1.0f,
-		 0.5f,  0.5f, -0.5f,  0.0f, 0.0f,1.0f,
-		 0.5f,  0.5f, -0.5f,  0.0f, 0.0f,1.0f,
-		 0.5f,  0.5f,  0.5f,  0.0f, 0.0f,1.0f,
-		 0.5f,  -0.5f, 0.5f, 0.0f, 0.0f,1.0f,
-      
-		-0.5f,  0.5f,  0.5f,  1.0f, 1.0f,0.0f,
-		-0.5f,  0.5f, -0.5f,  1.0f, 1.0f,0.0f,
-		-0.5f, -0.5f, -0.5f,  1.0f, 1.0f,0.0f,
-		-0.5f, -0.5f, -0.5f,  1.0f, 1.0f,0.0f,
-		-0.5f, -0.5f,  0.5f,  1.0f, 1.0f,0.0f,
-		-0.5f,  0.5f,  0.5f,  1.0f, 1.0f,0.0f,
-		
-		-0.5f, -0.5f, -0.5f, 0.0f, 1.0f,1.0f,
-		0.5f, -0.5f, -0.5f,  0.0f, 1.0f,1.0f,
-		0.5f, -0.5f,  0.5f,  0.0f, 1.0f,1.0f,
-		0.5f, -0.5f,  0.5f,  0.0f, 1.0f,1.0f,
-		-0.5f, -0.5f,  0.5f, 0.0f, 1.0f,1.0f,
-		-0.5f, -0.5f, -0.5f, 0.0f, 1.0f,1.0f,
-		
-		-0.5f,  0.5f, -0.5f, 1.0f, 0.2f,0.5f,
-		0.5f,  0.5f, -0.5f,  1.0f, 0.2f,0.5f,
-		0.5f,  0.5f,  0.5f,  1.0f, 0.2f,0.5f,
-		0.5f,  0.5f,  0.5f,  1.0f, 0.2f,0.5f,
-		-0.5f,  0.5f,  0.5f, 1.0f, 0.2f,0.5f,
-		-0.5f,  0.5f, -0.5f, 1.0f, 0.2f,0.5f,
-	};
+        // Left face (Laterales - Brillo 0.85)
+        -0.5f,  0.5f,  0.5f, 0.85f, 1.0f, 0.0f,
+        -0.5f,  0.5f, -0.5f, 0.85f, 1.0f, 0.0f,
+        -0.5f, -0.5f, -0.5f, 0.85f, 1.0f, 0.0f,
+        -0.5f, -0.5f, -0.5f, 0.85f, 1.0f, 0.0f,
+        -0.5f, -0.5f,  0.5f, 0.85f, 1.0f, 0.0f,
+        -0.5f,  0.5f,  0.5f, 0.85f, 1.0f, 0.0f,
 
+        // Right face (Laterales - Brillo 0.85)
+         0.5f,  0.5f,  0.5f, 0.85f, 0.2f, 0.5f,
+         0.5f,  0.5f, -0.5f, 0.85f, 0.2f, 0.5f,
+         0.5f, -0.5f, -0.5f, 0.85f, 0.2f, 0.5f,
+         0.5f, -0.5f, -0.5f, 0.85f, 0.2f, 0.5f,
+         0.5f, -0.5f,  0.5f, 0.85f, 0.2f, 0.5f,
+         0.5f,  0.5f,  0.5f, 0.85f, 0.2f, 0.5f,
 
+         // Top face (Superior - Brillo 1.0, la más iluminada)
+         -0.5f,  0.5f, -0.5f,  1.0f, 0.0f, 1.0f,
+          0.5f,  0.5f, -0.5f,  1.0f, 0.0f, 1.0f,
+          0.5f,  0.5f,  0.5f,  1.0f, 0.0f, 1.0f,
+          0.5f,  0.5f,  0.5f,  1.0f, 0.0f, 1.0f,
+         -0.5f,  0.5f,  0.5f,  1.0f, 0.0f, 1.0f,
+         -0.5f,  0.5f, -0.5f,  1.0f, 0.0f, 1.0f,
+
+         // Bottom face (Inferior - Brillo 0.6, la más oscura/sombra)
+         -0.5f, -0.5f, -0.5f, 0.6f, 1.0f, 1.0f,
+          0.5f, -0.5f, -0.5f, 0.6f, 1.0f, 1.0f,
+          0.5f, -0.5f,  0.5f, 0.6f, 1.0f, 1.0f,
+          0.5f, -0.5f,  0.5f, 0.6f, 1.0f, 1.0f,
+         -0.5f, -0.5f,  0.5f, 0.6f, 1.0f, 1.0f,
+         -0.5f, -0.5f, -0.5f, 0.6f, 1.0f, 1.0f,
+    };
 
 
 	GLuint VBO, VAO;
@@ -205,9 +162,9 @@ int main() {
 	glBindVertexArray(0); // Unbind VAO (it's always a good thing to unbind any buffer/array to prevent strange bugs)
 
 	
-	glm::mat4 projection=glm::mat4(1);
+	//glm::mat4 projection=glm::mat4(1);
 
-	projection = glm::perspective(45.0f, (GLfloat)screenWidth / (GLfloat)screenHeight, 0.1f, 100.0f);//FOV, Radio de aspecto,znear,zfar
+	//projection = glm::perspective(45.0f, (GLfloat)screenWidth / (GLfloat)screenHeight, 0.1f, 100.0f);//FOV, Radio de aspecto,znear,zfar
 	//projection = glm::ortho(0.0f, (GLfloat)screenWidth, 0.0f, (GLfloat)screenHeight, 0.1f, 1000.0f);//Izq,Der,Fondo,Alto,Cercania,Lejania
 	while (!glfwWindowShouldClose(window))
 	{
@@ -219,43 +176,94 @@ int main() {
 		glClearColor(1.0f, 1.0f, 1.0f, 1.0f);
 		glClear(GL_COLOR_BUFFER_BIT| GL_DEPTH_BUFFER_BIT);
 
+        // Usar el shader
+        ourShader.Use();
+        
+        //vista-camaraa
+        glm::mat4 view = glm::mat4(1.0f);
 
-		// Draw our first triangle
-		ourShader.Use();
-		glm::mat4 model=glm::mat4(1);
-		glm::mat4 view=glm::mat4(1);
-	
-	   view = glm::translate(view, glm::vec3(0.0f,0.0f,-12.0f));
-		model = glm::rotate( model, 0.5f, glm::vec3( 0.0f, 1.0f, 0.0f ) ); // use to compare orthographic and perspective projection
-		model = glm::scale(model, glm::vec3(4.0f, 3.0f, 1.0f));
-		//view = glm::translate( view, glm::vec3( screenWidth / 2, screenHeight / 5,-800.0f ) ); // use with orthographic projection
-		
-		GLint modelLoc = glGetUniformLocation(ourShader.Program, "model");
-		GLint viewLoc = glGetUniformLocation(ourShader.Program, "view");
-		GLint projecLoc = glGetUniformLocation(ourShader.Program, "projection");
-		
-		glUniformMatrix4fv(projecLoc, 1, GL_FALSE, glm::value_ptr(projection));
-		glUniformMatrix4fv(viewLoc, 1, GL_FALSE, glm::value_ptr(view));
-		glUniformMatrix4fv(modelLoc, 1, GL_FALSE, glm::value_ptr(model));
-		
-		           
-		glBindVertexArray(VAO);
-		glDrawArrays(GL_TRIANGLES, 0, 36);
-		///
-		model = glm::mat4(1);
-		model = glm::translate(model, glm::vec3(5.0f, 0.0f, 0.0f));
-		model = glm::rotate(model, 45.0f, glm::vec3(0.0f, 0.0f, 1.0f));
-		model = glm::scale(model, glm::vec3(8.0f, 1.0f, 5.0f));
-		glUniformMatrix4fv(modelLoc, 1, GL_FALSE, glm::value_ptr(model));
-		glDrawArrays(GL_TRIANGLES, 0, 36);
-		///
-		glBindVertexArray(0);
+        //alejamos toda la escena hacia el fondo(-Z) para que quepa en pantalla,
+        // y la bajamos un poco (-Y) para centrar la torre.
+        view = glm::translate(view, glm::vec3(0.0f, -4.0f, -35.0f));
 
+        // Rotamos en el eje X positivo. Esto inclina la parte superior de la torre 
+        // hacia atrás, simulando perfectamente que la estamos mirando desde abajo.
+        view = glm::rotate(view, glm::radians(15.0f), glm::vec3(1.0f, 0.0f, 0.0f));
 
-           
-		
-		
-		
+        // Rotamos en el eje Y para girar la torre y verla en un ángulo diagonal, así apreciamos todas las esquinas y sombras.
+        view = glm::rotate(view, glm::radians(45.0f), glm::vec3(0.0f, 1.0f, 0.0f));
+
+        glm::mat4 projection = glm::perspective(glm::radians(45.0f), (GLfloat)screenWidth / (GLfloat)screenHeight, 0.1f, 100.0f);
+
+        GLint modelLoc = glGetUniformLocation(ourShader.Program, "model");
+        GLint viewLoc = glGetUniformLocation(ourShader.Program, "view");
+        GLint projecLoc = glGetUniformLocation(ourShader.Program, "projection");
+        GLint colorLoc = glGetUniformLocation(ourShader.Program, "pastelColor");
+
+        glUniformMatrix4fv(projecLoc, 1, GL_FALSE, glm::value_ptr(projection));
+        glUniformMatrix4fv(viewLoc, 1, GL_FALSE, glm::value_ptr(view));
+
+        glBindVertexArray(VAO);
+
+        //  Definir escalas y colores
+        float base = 1.0f;
+        glm::vec3 scales[6] = {
+            glm::vec3(base * 1.0f), glm::vec3(base * 1.5f), glm::vec3(base * 2.0f),
+            glm::vec3(base * 2.5f), glm::vec3(base * 3.0f), glm::vec3(base * 4.0f)
+        };
+
+        glm::vec3 pastelColors[6] = {
+            glm::vec3(0.87f, 0.72f, 0.53f), glm::vec3(0.75f, 0.60f, 0.85f),
+            glm::vec3(0.60f, 0.80f, 0.95f), glm::vec3(0.95f, 0.70f, 0.80f),
+            glm::vec3(1.00f, 0.90f, 0.60f), glm::vec3(0.60f, 0.90f, 0.60f)
+        };
+
+        glm::vec3 currentPos = glm::vec3(0.0f, 0.0f, 0.0f);
+
+        //  Dibujar las cajas
+        for (int i = 0; i < 6; i++) {
+            glm::mat4 model = glm::mat4(1.0f);
+
+            if (i < 5) {
+                // Cajas 1 a 5
+                if (i > 0) {
+                    float offsetY = (scales[i - 1].y / 2.0f) + (scales[i].y / 2.0f);
+                    float offsetX = scales[i - 1].x / 2.0f;
+                    float offsetZ = scales[i - 1].z / 2.0f;
+                    currentPos += glm::vec3(offsetX, offsetY, offsetZ);
+                }
+
+                model = glm::translate(model, currentPos);
+                model = glm::rotate(model, glm::radians(15.0f * i), glm::vec3(0.0f, 1.0f, 0.0f));
+
+            }
+            else {
+                // SEXTA CAJA 
+                glm::vec3 topCenter5 = currentPos + glm::vec3(0.0f, scales[4].y / 2.0f, 0.0f);
+
+                // Distancia exacta del centro al vértice
+                float distToCorner = (scales[5].y * 1.73205f) / 2.0f;
+
+                // Subimos la caja para que el pico toque el centro exacto de la caja 5
+                glm::vec3 pos6 = topCenter5 + glm::vec3(0.0f, distToCorner, 0.0f);
+                model = glm::translate(model, pos6);
+
+                //esquina abajo
+                model = glm::rotate(model, glm::radians(-54.736f), glm::vec3(0.0f, 0.0f, 1.0f));
+                model = glm::rotate(model, glm::radians(45.0f), glm::vec3(0.0f, 1.0f, 0.0f));
+            }
+
+            model = glm::scale(model, scales[i]);
+
+            glUniformMatrix4fv(modelLoc, 1, GL_FALSE, glm::value_ptr(model));
+            if (colorLoc != -1) {
+                glUniform3fv(colorLoc, 1, glm::value_ptr(pastelColors[i]));
+            }
+
+            glDrawArrays(GL_TRIANGLES, 0, 36);
+        }
+
+        glBindVertexArray(0);
 		 
 		// Swap the screen buffers
 		glfwSwapBuffers(window);
--- ConfInicial/Shader/core.frag
+++ ConfInicial/Shader/core.frag
@@ -3,7 +3,13 @@ in vec3 ourColor;
 
 out vec4 color;
 
+in float brightness; // Recibimos el factor desde el Vertex Shader
+uniform vec3 pastelColor; 
+
 void main()
 {
-	color = vec4(ourColor, 1.0f);
+	//color = vec4(ourColor, 1.0f);
+	vec3 finalColor = pastelColor * brightness;
+
+    color = vec4(finalColor, 1.0f);
 }
